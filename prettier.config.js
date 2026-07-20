b0b243ec31187ce073f46d4dac4de7ffed1675f3
Felix <felixstar4@gmail.com>
2026-07-20 18:22:35 +0200

    Update globals, remove code formatters that don't work

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    tabWidth: 4,
-    useTabs: false,
-    printWidth: 1000, // Increase to avoid wrapping
-    bracketSameLine: true, // Optional: Keep closing brackets on same line where possible
-    singleQuote: true, // Optional: Consistent with your example
-};
